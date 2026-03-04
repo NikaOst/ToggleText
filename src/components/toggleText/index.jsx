4abc910facf291ca9b58bf3ec21ca7cef2250d39
VOstroverkh <veronikaostroverkh@gmail.com>
2026-03-04 12:49:35 +0100

    first commit

---
+++ src/components/toggleText/index.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+import React from 'react';
+
+function ToggleText() {
+  return <div className={styles.mainContainer}></div>;
+}
+export default ToggleText;
